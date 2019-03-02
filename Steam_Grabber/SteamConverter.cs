464535735c1a7762c1e364c0c013ce2a850098fd
Antlion <40786755+r3xq1@users.noreply.github.com>
2019-03-02 23:44:38 +0300

    Update SteamConverter.cs

--- Steam_Grabber/SteamConverter.cs
+++ Steam_Grabber/SteamConverter.cs
@@ -1,4 +1,4 @@
-﻿namespace Steam_Grabber
+namespace Steam_Grabber
 {
     using System;
     using System.Globalization;
@@ -21,32 +21,11 @@
         private static readonly long Num64 = 76561197960265728, Num32 = 76561197960265729;
         private static readonly int Number0 = 0;
 
-        public static long FromSteam2ToSteam64(string accountId)
-        {
-            if (!Regex.IsMatch(accountId, STEAM2))
-            {
-                return Number0;
-            }
-            return Num64 + Convert.ToInt64(accountId.Substring(0xA)) * 0x2 + Convert.ToInt64(accountId.Substring(0x8, 0x1));
-        }
+        public static long FromSteam2ToSteam64(string accountId) => !Regex.IsMatch(accountId, STEAM2) ? Number0 : Num64 + (Convert.ToInt64(accountId.Substring(0xA)) * 0x2) + Convert.ToInt64(accountId.Substring(0x8, 0x1));
 
-        public static long FromSteam32ToSteam64(long steam32)
-        {
-            if (steam32 < 0x1 || !Regex.IsMatch($"{STEAMPREFIX}{steam32.ToString(CultureInfo.InvariantCulture)}", STEAM32))
-            {
-                return Number0;
-            }
-            return steam32 + Num64;
-        }
+        public static long FromSteam32ToSteam64(long steam32) => steam32 < 0x1 || !Regex.IsMatch($"{STEAMPREFIX}{steam32.ToString(CultureInfo.InvariantCulture)}", STEAM32) ? Number0 : steam32 + Num64;
 
-        public static long FromSteam64ToSteam32(long communityId)
-        {
-            if (communityId < Num32 || !Regex.IsMatch(communityId.ToString(CultureInfo.InvariantCulture), STEAM64))
-            {
-                return Number0;
-            }
-            return communityId - Num64;
-        }
+        public static long FromSteam64ToSteam32(long communityId) => communityId < Num32 || !Regex.IsMatch(communityId.ToString(CultureInfo.InvariantCulture), STEAM64) ? Number0 : communityId - Num64;
 
         public static string FromSteam64ToSteam2(long communityId)
         {
@@ -56,12 +35,8 @@
             }
             communityId -= Num64;
             communityId -= communityId % 0x2;
-            var text = $"{STEAMPREFIX2}{communityId % 2}:{communityId / 0x2}";
-            if (!Regex.IsMatch(text, STEAM2))
-            {
-                return string.Empty;
-            }
-            return text;
+            string text = $"{STEAMPREFIX2}{communityId % 2}:{communityId / 0x2}";
+            return !Regex.IsMatch(text, STEAM2) ? string.Empty : text;
         }
     }
 }
